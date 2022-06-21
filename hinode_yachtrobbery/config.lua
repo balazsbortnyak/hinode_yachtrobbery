124d5063a64ef7ee3c292d9e9eeb75de9160e65b
Hinode Studios <78503694+imwillsee@users.noreply.github.com>
2022-06-21 17:36:54 +0200

    Add files via upload

---
+++ hinode_yachtrobbery/config.lua
@@ -0,0 +1,10 @@
+Config              = {}
+Config.Locale       = 'en'
+
+
+Config.LSPD = 0 -- How many cops need to be online
+
+Config.MinReward = 50000-- Minimum reward
+
+Config.MaxReward = 125000 -- Maximum reward
+
